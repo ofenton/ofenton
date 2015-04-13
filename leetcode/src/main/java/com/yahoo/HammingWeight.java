6606e03c8cff3f12bbadefbb68782ed7abea6838
ofenton <oliver.fenton@gmail.com>
2015-04-13 10:44:51 +0100

    add comment

--- leetcode/src/main/java/com/yahoo/HammingWeight.java
+++ leetcode/src/main/java/com/yahoo/HammingWeight.java
@@ -2,12 +2,16 @@ package com.yahoo;
 
 /**
  * Created on 3/29/15.
+ *
+ * For counting the number of bits set in an int
  */
 public class HammingWeight {
 
+    private static Integer NUM_BITS_IN_AN_INT = 32;
+
     public static int hammingWeight(int n) {
         int count = 0;
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < NUM_BITS_IN_AN_INT; i++) {
             if (n % 2 == 1) {
                 count++;
             }
